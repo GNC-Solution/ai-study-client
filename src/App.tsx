bc64a8c94105780180b4e73a13b6d6bae25d81f5
appano1 <thankfather@naver.com>
2021-06-08 13:15:53 +0900

    feat: Add route with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
