b10e7b8996ebe918ae36fcabfea1d735a0d3ea62
appano1 <thankfather@naver.com>
2021-06-08 14:40:42 +0900

    feat: Add Chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { QueryClientProvider } from 'react-query';
-import App from './App';
-import { queryClient } from './graphqlConfig';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClientProvider } from "react-query";
+import App from "./App";
+import { queryClient } from "./graphqlConfig";
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
