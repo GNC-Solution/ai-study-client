9eab1f2e8f5474bc8baf07271cf8b2b6c166912b
appano1 <thankfather@naver.com>
2021-06-08 13:15:30 +0900

    feat: Add components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+import NavigationBar from '../components/NavigationBar'
+
+export default function Home() {
+  return (
+    <div>
+      <NavigationBar />
+      <Header />
+    </div>
+  )
+}
