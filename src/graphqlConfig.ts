f37aaa47fad60ca37b121fa9edd52713387d25d7
appano1 <thankfather@naver.com>
2021-06-08 13:16:08 +0900

    feat: Add query client for react-query

---
+++ src/graphqlConfig.ts
@@ -0,0 +1,4 @@
+import { QueryClient } from "react-query";
+
+export const endPoint = "http://18.191.174.180:8100/graphql"
+export const queryClient = new QueryClient();
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClientProvider } from 'react-query';
 import App from './App';
+import { queryClient } from './graphqlConfig';
 
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
