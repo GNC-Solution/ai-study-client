e037a36d62af029096db6166f0bc42b08ff4127b
appano1 <thankfather@naver.com>
2021-06-18 10:44:24 +0900

    feat: Add get value from cookie function

--- src/hooks/useUserStore.tsx
+++ src/hooks/useUserStore.tsx
@@ -1,12 +1,23 @@
 import create from "zustand";
+import { combine } from "zustand/middleware";
 import { UserType } from "../model";
+import Cookies from "universal-cookie";
+import { USER_KEY } from "../constants";
 
 type UserState = {
   user: UserType | null;
   setUser: (user: UserType) => void;
 };
 
-export const useUserStore = create<UserState>((set) => ({
-  user: null,
-  setUser: (user) => set(() => ({ user: user }))
-}));
+function getDefaultValue() {
+  const cookies = new Cookies();
+  return {
+    user: cookies.get(USER_KEY),
+  };
+}
+
+export const useUserStore = create<UserState>(
+  combine(getDefaultValue(), (set) => ({
+    setUser: (user) => set(() => ({ user })),
+  }))
+);
