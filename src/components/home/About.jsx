48480f5723017bd1a41b8d986c457dcb8fd2b1ad
seyoung5744 <seyoung574458@gmail.com>
2021-06-29 21:12:05 +0900

    chore : add about comment

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -3,44 +3,31 @@ import React from "react";
 export default function About() {
   return (
     <>
-      
       <section id="about" className="about">
         <div className="container">
           <div className="row content">
             <div className="col-lg-6">
-              <h2>Eum ipsam laborum deleniti velitena</h2>
-              <h3>
-                Voluptatem dignissimos provident quasi corporis voluptates sit
-                assum perenda sruen jonee trave
-              </h3>
+              <h2>GNC Solution</h2>
+              <h3>AI 학습 몰입도 측정 솔루션</h3>
             </div>
             <div className="col-lg-6 pt-4 pt-lg-0">
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum
-              </p>
+              <p>Object Detection을 이용한 객체 인식 프로그램</p>
               <ul>
                 <li>
-                  <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                  ut aliquip ex ea commodo consequa
+                  <i className="ri-check-double-line"></i> Person Detection
                 </li>
                 <li>
-                  <i className="ri-check-double-line"></i> Duis aute irure dolor
-                  in reprehenderit in voluptate velit
+                  <i className="ri-check-double-line"></i> Cell Phone Detection
                 </li>
                 <li>
-                  <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in
+                  <i className="ri-check-double-line"></i> 5초 이상 자리를 비울
+                  시 자리비움으로 측정됨.
                 </li>
               </ul>
-              <p className="fst-italic">
+              {/* <p className="fst-italic">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
